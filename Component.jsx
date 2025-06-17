ecaf46d8d81c6caa51f824b9197e3f42d0c6688a
semihmayadagli <107646730+semihmayadagli@users.noreply.github.com>
2025-06-17 17:20:53 +0300

    Add files via upload

---
+++ Component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Component() {
+  return (
+    <div className="p-4 bg-white shadow rounded">
+      <p>AI destekli yangın raporu oluşturma arayüzüne hoş geldiniz.</p>
+    </div>
+  );
+}
